e2d8dbc154a0b961c43c0888c4823448702b0819
Jeroen Vandezande <Jeroen.Vandezande@spathi.be>
2013-04-28 20:28:13 +0200

    added Xtrareports 2012.2

---
+++ Echoes/Reports/XtraReports2012.2/source/XtraReport.pas
@@ -0,0 +1,25 @@
+namespace $rootnamespace$;
+
+interface
+
+uses
+  System,
+  System.Drawing,
+  System.Collections,
+  System.ComponentModel,
+  DevExpress.XtraReports.UI;
+
+type
+  $safeitemrootname$ = public partial class(DevExpress.XtraReports.UI.XtraReport)
+  public
+    constructor;
+  end;
+
+implementation
+
+constructor $safeitemrootname$;
+begin
+  InitializeComponent()
+end;
+
+end.
